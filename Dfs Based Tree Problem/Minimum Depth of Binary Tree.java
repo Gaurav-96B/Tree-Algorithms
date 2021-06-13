5a15f2efba620db1d8859f2715aedfb9098d37d8
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-06-13 12:49:22 +0530

    Update Minimum Depth of Binary Tree.java

--- Dfs Based Tree Problem/Minimum Depth of Binary Tree.java
+++ Dfs Based Tree Problem/Minimum Depth of Binary Tree.java
@@ -1,27 +1,23 @@
 class Solution {
     public int minDepth(TreeNode root) 
     {
-    if(root==null )
-    {
-        return 0;
-    }
-    if(root.left!=null&&root.right==null)
-    {
-       return 1+minDepth(root.left);
-    }
-    if(root.left==null&&root.right!=null)
-    {
-       return 1+minDepth(root.right);
-    }
-    if(root.left==null&&root.right==null)
-    {
-       return 1;
-    }
-    if(root.left!=null&&root.right!=null)
-    {
-       return 1+Math.min(minDepth(root.left),minDepth(root.right));
-    }
-    return 0;      
+        if(root==null)
+        {
+            return 0;
+        }
+        if(root.left==null&&root.right==null)
+        {
+            return 1;
+        }
+        if(root.left!=null&&root.right==null)
+        {
+           return 1+minDepth(root.left);
+        }
+        if(root.left==null&&root.right!=null)
+        {
+            return 1+minDepth(root.right);
+        }
+        return 1+Math.min(minDepth(root.left),minDepth(root.right));
     }
     
 }
