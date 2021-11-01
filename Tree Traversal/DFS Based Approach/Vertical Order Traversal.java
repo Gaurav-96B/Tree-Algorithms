3aeae34175d02c6f3578edd9e577a5e76cd85f24
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-11-01 13:02:18 +0530

    Update Vertical Order Traversal.java

--- Tree Traversal/DFS Based Approach/Vertical Order Traversal.java
+++ Tree Traversal/DFS Based Approach/Vertical Order Traversal.java
@@ -1,1 +1,34 @@
 
+class Solution
+{
+    static ArrayList <Integer> verticalOrder(Node root)
+    {
+        if(root==null)
+        {
+            return null;
+        }
+        Map<Integer, List<Integer>> map = new LinkedHashMap<>();
+        ArrayList<Integer>ans=new ArrayList<>();
+        diagonalSum(root,0,map);
+        ArrayList<Integer> keys = new ArrayList<Integer>(map.keySet());
+        Collections.sort(keys);
+        for(int k: keys){
+            for(int a: map.get(k)){
+                ans.add(a);
+            }
+        }
+        return ans;   
+    }
+    public static void diagonalSum(Node root,int diagonal, Map<Integer, List<Integer>> map )
+    {
+      if(root==null) 
+      {
+          return;
+      }
+      map.putIfAbsent(diagonal,new ArrayList<>());
+      map.get(diagonal).add(root.data);
+      diagonalSum(root.left, diagonal-1,map);
+      diagonalSum(root.right, diagonal+1,map);
+    }
+    
+}
