ceddabac8be0f51a9d549ff9ba095f8bdeaa10aa
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2022-06-27 22:35:03 +0530

    Update Preorder Traversal(Iterative Method).java

--- Tree Traversal/Preorder Traversal(Iterative Method).java
+++ Tree Traversal/Preorder Traversal(Iterative Method).java
@@ -1,31 +1,26 @@
 /* Itertaive Preorder traversal==Level order traversal only differnce (in preorder we use stack and in level order traversal we use Queue)*/
 
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer>ans=new ArrayList<>();
-        if(root==null)
-        {
-            return ans;
-        }
-        Stack<TreeNode>stack=new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty())
-        {
-            TreeNode node=stack.peek();
-            ans.add(node.val);
-            stack.pop();
-            
-            if(node.right!=null)
-            {
-                stack.push(node.right);
-            }
-            if(node.left!=null)
-            {
-               stack.push(node.left);
-            }  
-        }
-        return ans;
-        
+static ArrayList<Integer> preorder(Node root)
+    {
+      ArrayList<Integer>ans=new ArrayList<>();
+      if(root==null)
+      {
+          return ans;
+      }
+      Stack<Node>stack=new Stack<>();
+      stack.push(root);
+      while(!stack.isEmpty())
+      {
+          Node n=stack.pop();
+          ans.add(n.data);
+          if(n.right!=null)
+          {
+              stack.push(n.right);
+          }
+          if(n.left!=null)
+          {
+              stack.push(n.left);
+          }
+      }
+      return ans;
     }
-   
-}
