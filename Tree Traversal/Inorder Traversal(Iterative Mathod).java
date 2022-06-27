17c4e14c782e80ea7d5f2d8ed33c14978bcc6791
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2022-06-27 14:18:26 +0530

    Update Inorder Traversal(Iterative Mathod).java

--- Tree Traversal/Inorder Traversal(Iterative Mathod).java
+++ Tree Traversal/Inorder Traversal(Iterative Mathod).java
@@ -1,22 +1,25 @@
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer>ans=new ArrayList<>();
-        Stack<TreeNode>stack=new Stack<>();
-        TreeNode curr=root;
-        while(!stack.isEmpty()||curr!=null)
-        {
-            while(curr!=null)
-            {
-                stack.push(curr);
-                curr=curr.left;
-            }
-            curr=stack.peek();
-            ans.add(curr.val);
-            stack.pop();
-            
-            curr=curr.right;  
-        }
-        return ans;
-       
+ ArrayList<Integer> inOrder(Node root) {
+      ArrayList<Integer>ans=new ArrayList<>();
+      if(root==null)  
+      {
+        return ans; 
+      }
+      Stack<Node>stack=new Stack<>();
+      Node curr=root;
+      while(!stack.isEmpty()||curr!=null)
+      {
+          if(curr!=null)
+          {
+              stack.push(curr);
+              curr=curr.left;
+          }
+          else
+          {
+              curr=stack.pop();
+              ans.add(curr.data);
+              curr=curr.right;
+          }
+      }
+      return ans;
+     
     }
-}
